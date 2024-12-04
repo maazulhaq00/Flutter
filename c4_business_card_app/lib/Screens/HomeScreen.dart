ecadb79fe20e70ec47afe493c77149bedc952600
maaz <maazulhaq00@gmail.com>
2024-12-04 20:59:39 +0500

    icons & text style

--- c4_business_card_app/lib/Screens/HomeScreen.dart
+++ c4_business_card_app/lib/Screens/HomeScreen.dart
@@ -15,8 +15,43 @@ class HomeScreen extends StatelessWidget {
             radius: 50,
             backgroundImage: AssetImage('images/profile.jpg'),
           ),
-          Text("Maaz Ul Haq"),
-          Text("Software Engineer")
+          Text(
+            "Maaz Ul Haq",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico'),
+          ),
+          Text(
+            "Software Engineer",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                letterSpacing: 2,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Source Code Pro'),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+            color: Colors.white,
+            child: Row(children: [
+              Icon(
+                Icons.phone,
+                color: Colors.deepPurple.shade500,
+              ),
+              SizedBox(width: 10.0,),
+              Text(
+                '0311-1234567',
+                style: TextStyle(
+                  color: Colors.deepPurple.shade800,
+                  fontSize: 20,
+                  fontFamily: 'Source Code Pro',
+                ),
+              )
+            ]),
+          )
         ],
       ),
     );
